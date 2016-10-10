4ec040b42882f586ac0cda78b677c912e9934a41
ChrisAshton <christopherashton@g.harvard.edu>
2016-10-10 16:47:39 -0400

    add pycharm project folder and .py setup files

---
+++ vagrant/pycharmFullstack/sessionStarter.py
@@ -0,0 +1,14 @@
+from database_setup import Integer, Column, ForeignKey, String, relationship
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+
+session = DBSession()
+
+
+
