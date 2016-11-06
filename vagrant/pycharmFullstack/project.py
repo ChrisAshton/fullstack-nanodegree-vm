afc35f9fe3ecb5c0ec3a193bee023c5604deb1cf
ChrisAshton <christopherashton@g.harvard.edu>
2016-11-06 18:31:23 -0500

    Add delete menu item

--- vagrant/pycharmFullstack/project.py
+++ vagrant/pycharmFullstack/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash
 
 app = Flask(__name__)
 
@@ -25,37 +25,47 @@ def restaurantMenu(restaurant_id):
 def newMenuItem(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     if request.method == 'POST':
-        newItem = MenuItem(name = request.form['name'], restaurant_id=restaurant_id)
+        newItem = MenuItem(name=request.form['name'], restaurant_id=restaurant_id)
         session.add(newItem)
         session.commit()
+        flash("new menu item created: %s. (be sure to edit the description and price)" % newItem.name)
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
 
     else:
-        return render_template('newmenuitem.html', restaurant_id=restaurant_id, restaurantname=restaurant.name)
+        return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 
 
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit_menu_item', methods=['GET','POST'])
 def editMenuItem(restaurant_id, menu_id):
     menu_item = session.query(MenuItem).filter_by(id= menu_id).one()
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    if request.method=='POST':
-        if request.form['newname']:
-            menu_item.name = request.form['newname']
+    if request.method == 'POST':
+        if request.form['name']:
+            menu_item.name = request.form['name']
+        if request.form["description"]:
+            menu_item.description = request.form["description"]
+        if request.form["price"]:
+            menu_item.price = request.form["price"]
         session.add(menu_item)
         session.commit()
+        flash("%s edited" % menu_item.name)
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
-        return render_template('editmenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id)
+        return render_template('editmenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, menu_item=menu_item, restaurant=restaurant)
 
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete_menu_item')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete_menu_item', methods = ['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
     menu_item = session.query(MenuItem).filter_by(id= int(menu_id)).one()
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    output = ""
-    output += "this a page to delete %s" % menu_item.name
-    output += " at %s" % restaurant.name
-    return output
+    if request.method == 'POST':
+        session.delete(menu_item)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('deletemenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, item=menu_item, restaurant=restaurant)
+
 
 if __name__ == '__main__':
+    app.secret_key = 'SUPER_SECRET_KEY'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
